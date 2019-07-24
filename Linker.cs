3215a5a6d1238a94c5ab6cd9c769170cb4bd50c3
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2019-07-24 14:03:05 -0400

    abt to rework linking

--- Linker.cs
+++ Linker.cs
@@ -27,43 +27,24 @@ public class Linker : MonoBehaviour {
     Vector2 pos = triangle[index];
     legA = -Vector2.SignedAngle(Vector2.up, triangle[(index + 1) % triangle.Length] - triangle[index]);
     legB = -Vector2.SignedAngle(Vector2.up, triangle[(index + 2) % triangle.Length] - triangle[index]);
-    
-    // Debug.Log(legA + " " + legB);
-    // Debug.Log(triangle[0] + " " + triangle[1] + " " + triangle[2]);
-  }
-  
-  private bool CheckValid() {
-    // Collider2D[] cols = new Collider2D[1];
-    // trigonCol.GetContacts(cols);
-    // Debug.Log(cols[0] == null);
-    // return (cols[0] == null);
-    
-    return true;
   }
   
   private bool CheckAngle(float ang) {
     // transform.parent.RotateAround(transform.position, Vector3.forward, ang);
-    // bool valid;
-    // if (valid = CheckValid()) {
-      this.ang = ang;
+    this.ang = ang;
     return true;
-    // }
-    // transform.parent.RotateAround(transform.position, Vector3.forward, -ang);
-    // return valid;
   }
   
+  private Vector2[] ghost = new Vector2[3];
   private void Validate(float ang, Vector2 dir) {
-    Vector2[] ghost = new Vector2[3];
     System.Array.Copy(triangle, ghost, 3);
+    
     for (int i = 0; i < ghost.Length; i++) {
-      
-      ghost[i] -= ghost[index];
-      Debug.Log(ghost[i]);
       ghost[i] = Quaternion.AngleAxis(transform.rotation.eulerAngles.z + ang, Vector3.forward) * ghost[i];
       ghost[i] += (Vector2)transform.position;
       ghost[i] += dir;
       
-      Instantiate(debugPoint, ghost[i], Quaternion.identity);
+      Instantiate(debugPoint, ghost[i] + (Vector2)transform.position, Quaternion.identity);
     }
     bool valid = true;
   }
@@ -71,9 +52,6 @@ public class Linker : MonoBehaviour {
   private void Awake() {
     trigonCol = transform.parent.GetComponent<Collider2D>();
     trigonAnimator = transform.parent.GetComponent<Animator>();
-  }
-  
-  private void OnTriggerEnter2D() {
   }
   
   private void OnTriggerStay2D(Collider2D col) {
@@ -89,9 +67,11 @@ public class Linker : MonoBehaviour {
     
     Transform parent = transform.parent;
     parent.position += (Vector3)dir;
+    // transform.parent.RotateAround(transform.position, Vector3.forward, 90f);
     if (Mathf.Abs(ab) < Mathf.Abs(ba)) {
       if (CheckAngle(ab) || CheckAngle(ba)) {
         this.dir = dir;
+        Validate(ang, dir);
       } else {
         this.dir = Vector2.negativeInfinity;
         ang = Mathf.NegativeInfinity;
@@ -99,14 +79,14 @@ public class Linker : MonoBehaviour {
     } else {
       if (CheckAngle(ba) || CheckAngle(ab)) {
         this.dir = dir;
+        Validate(ang, dir);
       } else {
         this.dir = Vector2.negativeInfinity;
         ang = Mathf.NegativeInfinity;
       }
     }
     parent.position -= (Vector3)dir;
-    
-    Validate(ang, dir);
+    // transform.parent.RotateAround(transform.position, Vector3.forward, -90f);
   }
   
   private void OnTriggerExit2D(Collider2D col) {
